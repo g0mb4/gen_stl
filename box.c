43d9b9b593ecbcb3e47d2a60da8aa65816834703
g0mb4 <gomba007@gmail.com>
2022-07-01 09:57:22 +0200

    Cleanup code.

--- box.c
+++ box.c
@@ -6,108 +6,109 @@
 #include <string.h>
 
 /*
-    creates a (0, 0, 0) centered box
+    creates an axis-aligned (0, 0, 0) centered box
 */
 facet* box(int* len, double lx, double ly, double lz, int mask)
 {
     check(mask >= 0, "mask must be non-negative\n");
 
-    int size = 6 * 2 * sizeof(facet); // theoretical maximum
+    int max_size = 6 * 2 * sizeof(facet); // theoretical maximum
 
-    facet* list = (facet*)malloc(size);
+    facet* list = (facet*)malloc(max_size);
 
     check(list != NULL, "unable to allocate memory\n");
 
-    memset(list, 0, size);
-    *len = 0;
+    memset(list, 0, max_size);
+    int index = 0;
 
     double w = lx / 2;
     double h = ly / 2;
     double d = lz / 2;
 
     if (mask & BOX_LEFT) {
-        list[*len].normal = v3(-1.0, 0.0, 0.0);
-        list[*len].vertices[0] = v3(-w, h, d);
-        list[*len].vertices[1] = v3(-w, h, -d);
-        list[*len].vertices[2] = v3(-w, -h, d);
-        ++*len;
+        list[index].normal = v3(-1.0, 0.0, 0.0);
+        list[index].vertices[0] = v3(-w, h, d);
+        list[index].vertices[1] = v3(-w, h, -d);
+        list[index].vertices[2] = v3(-w, -h, d);
+        ++index;
 
-        list[*len].normal = v3(-1.0, 0.0, 0.0);
-        list[*len].vertices[0] = v3(-w, -h, d);
-        list[*len].vertices[1] = v3(-w, h, -d);
-        list[*len].vertices[2] = v3(-w, -h, -d);
-        ++*len;
+        list[index].normal = v3(-1.0, 0.0, 0.0);
+        list[index].vertices[0] = v3(-w, -h, d);
+        list[index].vertices[1] = v3(-w, h, -d);
+        list[index].vertices[2] = v3(-w, -h, -d);
+        ++index;
     }
 
     if (mask & BOX_RIGHT) {
-        list[*len].normal = v3(1.0, 0.0, 0.0);
-        list[*len].vertices[0] = v3(w, h, -d);
-        list[*len].vertices[1] = v3(w, h, d);
-        list[*len].vertices[2] = v3(w, -h, -d);
-        ++*len;
+        list[index].normal = v3(1.0, 0.0, 0.0);
+        list[index].vertices[0] = v3(w, h, -d);
+        list[index].vertices[1] = v3(w, h, d);
+        list[index].vertices[2] = v3(w, -h, -d);
+        ++index;
 
-        list[*len].normal = v3(1.0, 0.0, 0.0);
-        list[*len].vertices[0] = v3(w, -h, -d);
-        list[*len].vertices[1] = v3(w, h, d);
-        list[*len].vertices[2] = v3(w, -h, d);
-        ++*len;
+        list[index].normal = v3(1.0, 0.0, 0.0);
+        list[index].vertices[0] = v3(w, -h, -d);
+        list[index].vertices[1] = v3(w, h, d);
+        list[index].vertices[2] = v3(w, -h, d);
+        ++index;
     }
 
     if (mask & BOX_TOP) {
-        list[*len].normal = v3(0.0, 0.0, 1.0);
-        list[*len].vertices[0] = v3(w, h, d);
-        list[*len].vertices[1] = v3(-w, h, d);
-        list[*len].vertices[2] = v3(w, -h, d);
-        ++*len;
+        list[index].normal = v3(0.0, 0.0, 1.0);
+        list[index].vertices[0] = v3(w, h, d);
+        list[index].vertices[1] = v3(-w, h, d);
+        list[index].vertices[2] = v3(w, -h, d);
+        ++index;
 
-        list[*len].normal = v3(0.0, 0.0, 1.0);
-        list[*len].vertices[0] = v3(w, -h, d);
-        list[*len].vertices[1] = v3(-w, h, d);
-        list[*len].vertices[2] = v3(-w, -h, d);
-        ++*len;
+        list[index].normal = v3(0.0, 0.0, 1.0);
+        list[index].vertices[0] = v3(w, -h, d);
+        list[index].vertices[1] = v3(-w, h, d);
+        list[index].vertices[2] = v3(-w, -h, d);
+        ++index;
     }
 
     if (mask & BOX_BOTTOM) {
-        list[*len].normal = v3(0.0, 0.0, -1.0);
-        list[*len].vertices[0] = v3(-w, h, -d);
-        list[*len].vertices[1] = v3(w, h, -d);
-        list[*len].vertices[2] = v3(-w, -h, -d);
-        ++*len;
+        list[index].normal = v3(0.0, 0.0, -1.0);
+        list[index].vertices[0] = v3(-w, h, -d);
+        list[index].vertices[1] = v3(w, h, -d);
+        list[index].vertices[2] = v3(-w, -h, -d);
+        ++index;
 
-        list[*len].normal = v3(0.0, 0.0, -1.0);
-        list[*len].vertices[0] = v3(-w, -h, -d);
-        list[*len].vertices[1] = v3(w, h, -d);
-        list[*len].vertices[2] = v3(w, -h, -d);
-        ++*len;
+        list[index].normal = v3(0.0, 0.0, -1.0);
+        list[index].vertices[0] = v3(-w, -h, -d);
+        list[index].vertices[1] = v3(w, h, -d);
+        list[index].vertices[2] = v3(w, -h, -d);
+        ++index;
     }
 
     if (mask & BOX_FRONT) {
-        list[*len].normal = v3(0.0, -1.0, 0.0);
-        list[*len].vertices[0] = v3(w, -h, -d);
-        list[*len].vertices[1] = v3(w, -h, d);
-        list[*len].vertices[2] = v3(-w, -h, -d);
-        ++*len;
+        list[index].normal = v3(0.0, -1.0, 0.0);
+        list[index].vertices[0] = v3(w, -h, -d);
+        list[index].vertices[1] = v3(w, -h, d);
+        list[index].vertices[2] = v3(-w, -h, -d);
+        ++index;
 
-        list[*len].normal = v3(0.0, -1.0, 0.0);
-        list[*len].vertices[0] = v3(-w, -h, -d);
-        list[*len].vertices[1] = v3(w, -h, d);
-        list[*len].vertices[2] = v3(-w, -h, d);
-        ++*len;
+        list[index].normal = v3(0.0, -1.0, 0.0);
+        list[index].vertices[0] = v3(-w, -h, -d);
+        list[index].vertices[1] = v3(w, -h, d);
+        list[index].vertices[2] = v3(-w, -h, d);
+        ++index;
     }
 
     if (mask & BOX_BACK) {
-        list[*len].normal = v3(0.0, 1.0, 0.0);
-        list[*len].vertices[0] = v3(w, h, d);
-        list[*len].vertices[1] = v3(w, h, -d);
-        list[*len].vertices[2] = v3(-w, h, d);
-        ++*len;
+        list[index].normal = v3(0.0, 1.0, 0.0);
+        list[index].vertices[0] = v3(w, h, d);
+        list[index].vertices[1] = v3(w, h, -d);
+        list[index].vertices[2] = v3(-w, h, d);
+        ++index;
 
-        list[*len].normal = v3(0.0, 1.0, 0.0);
-        list[*len].vertices[0] = v3(-w, h, d);
-        list[*len].vertices[1] = v3(w, h, -d);
-        list[*len].vertices[2] = v3(-w, h, -d);
-        ++*len;
+        list[index].normal = v3(0.0, 1.0, 0.0);
+        list[index].vertices[0] = v3(-w, h, d);
+        list[index].vertices[1] = v3(w, h, -d);
+        list[index].vertices[2] = v3(-w, h, -d);
+        ++index;
     }
 
+    *len = index;
     return list;
 }
--- cylinder.c
+++ cylinder.c
@@ -11,29 +11,29 @@
 #endif
 
 /*
-    creates a (0, 0, 0) centered cylinder
+    creates an axis-aligned (0, 0, 0) centered cylinder
 */
 facet* cylinder(int* len, double r, double h, double N, int mask)
 {
     check(mask >= 0, "mask must be non-negative\n");
 
-    int size = N * 2 * 2 * sizeof(facet); // theoretical maximum
+    int max_size = N * 2 * 2 * sizeof(facet); // theoretical maximum
 
-    facet* list = (facet*)malloc(size);
+    facet* list = (facet*)malloc(max_size);
 
     check(list != NULL, "unable to allocate memory\n");
 
-    memset(list, 0, size);
-    *len = 0;
+    memset(list, 0, max_size);
+    int index = 0;
 
     vector3* ring = malloc(N * sizeof(vector3));
 
     check(ring != NULL, "unable to allocate ring\n");
 
-    double da = (2 * M_PI) / (double)(N - 1);
+    double angle_increment = (2 * M_PI) / (double)(N - 1);
     for (int i = 0; i < N; ++i) {
-        double x = r * cos(i * da);
-        double y = r * sin(i * da);
+        double x = r * cos(i * angle_increment);
+        double y = r * sin(i * angle_increment);
 
         ring[i].x = x;
         ring[i].y = y;
@@ -43,73 +43,73 @@ facet* cylinder(int* len, double r, double h, double N, int mask)
         int pair = i == 0 ? N - 1 : i - 1;
 
         if (mask & CYLINDER_TOP) {
-            list[*len].normal = v3(0.0, 0.0, 1.0);
+            list[index].normal = v3(0.0, 0.0, 1.0);
 
-            list[*len].vertices[0] = v3(0.0, 0.0, h / 2);
+            list[index].vertices[0] = v3(0.0, 0.0, h / 2);
 
-            list[*len].vertices[1].x = ring[i].x;
-            list[*len].vertices[1].y = ring[i].y;
-            list[*len].vertices[1].z = h / 2;
+            list[index].vertices[1].x = ring[i].x;
+            list[index].vertices[1].y = ring[i].y;
+            list[index].vertices[1].z = h / 2;
 
-            list[*len].vertices[2].x = ring[pair].x;
-            list[*len].vertices[2].y = ring[pair].y;
-            list[*len].vertices[2].z = h / 2;
-            ++*len;
+            list[index].vertices[2].x = ring[pair].x;
+            list[index].vertices[2].y = ring[pair].y;
+            list[index].vertices[2].z = h / 2;
+            ++index;
         }
 
         if (mask & CYLINDER_BOTTOM) {
-            list[*len].normal = v3(0.0, 0.0, -1.0);
+            list[index].normal = v3(0.0, 0.0, -1.0);
 
-            list[*len].vertices[0] = v3(0.0, 0.0, -h / 2);
+            list[index].vertices[0] = v3(0.0, 0.0, -h / 2);
 
-            list[*len].vertices[1].x = ring[i].x;
-            list[*len].vertices[1].y = ring[i].y;
-            list[*len].vertices[1].z = -h / 2;
+            list[index].vertices[1].x = ring[i].x;
+            list[index].vertices[1].y = ring[i].y;
+            list[index].vertices[1].z = -h / 2;
 
-            list[*len].vertices[2].x = ring[pair].x;
-            list[*len].vertices[2].y = ring[pair].y;
-            list[*len].vertices[2].z = -h / 2;
-            ++*len;
+            list[index].vertices[2].x = ring[pair].x;
+            list[index].vertices[2].y = ring[pair].y;
+            list[index].vertices[2].z = -h / 2;
+            ++index;
         }
 
         if (mask & CYLINDER_SIDE) {
-
             /* FIXME */
             vector3 n = v3(1.0, 0.0, 0.0);
 
-            list[*len].normal = n;
+            list[index].normal = n;
 
-            list[*len].vertices[0].x = ring[i].x;
-            list[*len].vertices[0].y = ring[i].y;
-            list[*len].vertices[0].z = h / 2;
+            list[index].vertices[0].x = ring[i].x;
+            list[index].vertices[0].y = ring[i].y;
+            list[index].vertices[0].z = h / 2;
 
-            list[*len].vertices[1].x = ring[pair].x;
-            list[*len].vertices[1].y = ring[pair].y;
-            list[*len].vertices[1].z = h / 2;
+            list[index].vertices[1].x = ring[pair].x;
+            list[index].vertices[1].y = ring[pair].y;
+            list[index].vertices[1].z = h / 2;
 
-            list[*len].vertices[2].x = ring[i].x;
-            list[*len].vertices[2].y = ring[i].y;
-            list[*len].vertices[2].z = -h / 2;
+            list[index].vertices[2].x = ring[i].x;
+            list[index].vertices[2].y = ring[i].y;
+            list[index].vertices[2].z = -h / 2;
 
-            ++*len;
+            ++index;
 
-            list[*len].normal = n;
+            list[index].normal = n;
 
-            list[*len].vertices[0].x = ring[pair].x;
-            list[*len].vertices[0].y = ring[pair].y;
-            list[*len].vertices[0].z = h / 2;
+            list[index].vertices[0].x = ring[pair].x;
+            list[index].vertices[0].y = ring[pair].y;
+            list[index].vertices[0].z = h / 2;
 
-            list[*len].vertices[1].x = ring[pair].x;
-            list[*len].vertices[1].y = ring[pair].y;
-            list[*len].vertices[1].z = -h / 2;
+            list[index].vertices[1].x = ring[pair].x;
+            list[index].vertices[1].y = ring[pair].y;
+            list[index].vertices[1].z = -h / 2;
 
-            list[*len].vertices[2].x = ring[i].x;
-            list[*len].vertices[2].y = ring[i].y;
-            list[*len].vertices[2].z = -h / 2;
+            list[index].vertices[2].x = ring[i].x;
+            list[index].vertices[2].y = ring[i].y;
+            list[index].vertices[2].z = -h / 2;
 
-            ++*len;
+            ++index;
         }
     }
 
+    *len = index;
     return list;
 }
